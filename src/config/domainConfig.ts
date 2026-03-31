8f05ed7da1bb97959218fad270c4a94cd9d6d9de
Carlos Rodriguez <alos1212@gmail.com>
2026-03-31 15:06:27 -0500

    ajustes

--- src/config/domainConfig.ts
+++ src/config/domainConfig.ts
@@ -5,9 +5,7 @@ export type DomainConfigEntry = {
 
 const domainConfig: Record<string, DomainConfigEntry> = {
   localhost: { page: "1", countryId: "1" },
-  "dominio2.com": { page: "2", countryId: "1" },
-  "sistema.wowassistance.com": { page: "1", countryId: "1" },
-  "agentes.wowassistance.com": { page: "1", countryId: "1" },
+  "vualacrm.com": { page: "1", countryId: "1" },
 };
 
 export const getDomainConfig = (hostname: string): DomainConfigEntry => {
